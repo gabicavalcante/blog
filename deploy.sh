8e36c0d8e143c80b9bacbb58ee62744dbdaa0221
Gabriela Cavalcante da Silva <gabicavalcantesilva@gmail.com>
2020-09-23 19:41:41 -0300

    fix post: python django cicd

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,23 @@
 # If a command fails then the deploy stops
 set -e
 
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+printf "Deploying updates to GitHub..."
 
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
 
 # Go To Public folder
 cd public
